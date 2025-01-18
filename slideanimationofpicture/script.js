865c64536383e9c45f76f56939fcf3d0b8405f7f
Sayyed Amaan Ali <130714872+sayyedali2@users.noreply.github.com>
2025-01-18 19:21:32 +0530

    Update script.js

--- slideanimationofpicture/script.js
+++ slideanimationofpicture/script.js
@@ -1,49 +1,20 @@
-const addButton = document.getElementById("add");
-const notes = JSON.parse(localStorage.getItem("notes"));
+const boxes = document.querySelectorAll('.box');
+function removeAllProperties(){
+  boxes.forEach((e)=>{
+    e.style.width='6%'
+    e.style.borderRadius='45px'
+    e.firstElementChild.style.borderRadius='45px'
+  })}
 
-const updateLocalStorage = () => {
-  const notesText = document.querySelectorAll("textarea");
-  const notes = [];
-  notesText.forEach((note) => notes.push(note.value));
-  localStorage.setItem("notes", JSON.stringify(notes));
-};
-
-const addNewNote = (text = "") => {
-  const note = document.createElement("div");
-  note.classList.add("note");
-  note.innerHTML = `
-  <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-  </div>
-  <div class="main ${text ? "" : "hidden"}"></div>
-  <textarea class="${text ? "hidden" : ""}"></textarea>`;
-
-  const editButton = note.querySelector(".edit");
-  const deleteButton = note.querySelector(".delete");
-  const main = note.querySelector(".main");
-  const textArea = note.querySelector("textarea");
-  textArea.value = text;
-  main.innerHTML = marked(text);
-
-  deleteButton.addEventListener("click", () => {
-    note.remove();
-    updateLocalStorage();
-  });
-  editButton.addEventListener("click", () => {
-    main.classList.toggle("hidden");
-    textArea.classList.toggle("hidden");
-  });
-  textArea.addEventListener("input", (e) => {
-    const { value } = e.target;
-    main.innerHTML = marked(value);
-    updateLocalStorage();
-  });
-  document.body.appendChild(note);
-};
-
-addButton.addEventListener("click", () => addNewNote());
-
-if (notes) {
-  notes.forEach((note) => addNewNote(note));
+function addPropetie(e){
+  e.style.width="70%"
+  e.style.borderRadius='0px'
+ e.firstElementChild.style.borderRadius='0px'
 }
+
+boxes.forEach((e)=>{
+  e.addEventListener('click', ()=>{
+    removeAllProperties();
+    addPropetie(e);
+  })
+})
